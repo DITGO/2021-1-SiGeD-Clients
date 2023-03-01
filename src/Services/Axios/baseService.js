ca585a741cde9e87086670218eb2bf1a1eade9ac
DITGO <88164111+DITGO@users.noreply.github.com>
2023-03-01 15:16:13 -0300

    Update baseService.js

--- src/Services/Axios/baseService.js
+++ src/Services/Axios/baseService.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const { USERS_URL, DEMANDS_URL } = process.env;
 
 const APIUsers = axios.create({
-  baseURL: `http://${USERS_URL}/`,
+  baseURL: `http://${USERS_URL}:3001/`,
 });
 
 const APIDemands = axios.create({
-  baseURL: `http://${DEMANDS_URL}/`,
+  baseURL: `http://${DEMANDS_URL}:3003/`,
 });
 
 module.exports = {
